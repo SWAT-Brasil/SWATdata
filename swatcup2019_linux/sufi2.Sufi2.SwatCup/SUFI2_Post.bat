fa57a77f3b468413a37edb45ae161995853ff32a
jairo <jairo.rotava@gmail.com>
2020-04-23 15:19:02 -0300

    desativa 95ppu pois nao funciona em linux

--- swatcup2019_linux/sufi2.Sufi2.SwatCup/SUFI2_Post.bat
+++ swatcup2019_linux/sufi2.Sufi2.SwatCup/SUFI2_Post.bat
@@ -1,4 +1,6 @@
 ./SUFI2_goal_fn.exe 
-./SUFI2_new_pars.exe 
-./SUFI2_95ppu.exe 
-./SUFI2_95ppu_beh.exe
+./SUFI2_new_pars.exe
+# Estes comandos nao funcionam no linux, geram erro
+# O erro gerado eh Segmentation fault (core dumped)
+#./SUFI2_95ppu.exe
+#./SUFI2_95ppu_beh.exe
